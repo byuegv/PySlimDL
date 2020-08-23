8857fb6882f46d083cee907b5b55434b2e716800
wdaemon <byuegv@163.com>
2020-08-23 12:34:04 +0800

    modify run.sh:

--- src/run.sh
+++ src/run.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
+# flush redis
 redis-cli flushall
+
 _edge=${2}
+_log=${3}
+
 #for idx in $(seq 1 ${_edge}) ; do
 
-  python ${1} --dataset "/workspace" --edgenum ${_edge} --gpu --noncriticalremove --fixedratio "0.7" &
-  sleep 10s
+  sleep 10s && python ${1} --dataset "/workspace" --edgenum ${_edge} --gpu --fixedratio "0.7" &
 
-  python ${1} --dataset "/workspace" --edgenum ${_edge}  --noncriticalremove --fixedratio "0.7" &
-  #sleep ${idx}s
-  sleep 5s
+  sleep 5s && python ${1} --dataset "/workspace" --edgenum ${_edge} --gpu  --fixedratio "0.7" &
 
-  python ${1} --dataset "/workspace" --edgenum ${_edge} --gpu --noncriticalremove --fixedratio "0.7" &
-  sleep 10s
+  sleep 7s && python ${1} --dataset "/workspace" --edgenum ${_edge} --noncriticalremove --fixedratio "0.3" &
 
-  python ${1} --dataset "/workspace" --edgenum ${_edge}  --noncriticalremove --fixedratio "0.7" &
-  #sleep ${idx}s
+  sleep 13s && python ${1} --dataset "/workspace" --edgenum ${_edge}  --noncriticalremove --fixedratio "0.3" &
 
- # python ${1} --dataset "/workspace" --edgenum ${_edge} --gpu --noncriticalremove --fixedratio "0.7" &
-  
 #done
 wait
+
+if [ -d ${_log} ]; then
+    mv ./2020*.log ${_log}
+else
+    mkdir -p ${_log}
+    mv ./2020*.log ${_log}
+fi
 
 echo "Finished"
